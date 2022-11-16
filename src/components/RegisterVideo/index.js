1879cf2e4c874a0dfb39f96cd53d8d1598103ad5
Willian K <billmetal9@gmail.com>
2022-11-16 13:58:16 -0300

    Acertando Variáveis de ambiente

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -20,13 +20,7 @@ function useForm(propsDoForm){
     };
 }
 
-const PROJECT_URL = process.env.URL;
-const PUBLIC_KEY = process.env.SUPABASE_KEY;
-
-console.log("URL = " + PROJECT_URL);
-console.log("APIKEY = " + PUBLIC_KEY);
-
-const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
+const supabase = createClient(process.env.PROJECT_URL, process.env.PUBLIC_KEY);
 
 export default function RegisterVideo({ loadVideos }){
     const [formVisivel, setFormVisivel] = useState(false);
--- src/services/videoService.js
+++ src/services/videoService.js
@@ -1,12 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const PROJECT_URL = process.env.URL;
-const PUBLIC_KEY = process.env.SUPABASE_KEY;
-
-console.log("URL = " + PROJECT_URL);
-console.log("APIKEY = " + PUBLIC_KEY);
-
-const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
+const supabase = createClient(process.env.PROJECT_URL, process.env.PUBLIC_KEY);
 
 export default function videoService(){
     return {
